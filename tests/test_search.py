22a0050132d700fb6acec3316e76602285f3e2ac
AndyLPK247 <AndyLPK247@gmail.com>
2020-04-10 01:59:25 -0400

    Section 5 code

--- tests/test_search.py
+++ tests/test_search.py
@@ -27,6 +27,3 @@ def test_basic_duckduckgo_search(browser):
   titles = result_page.result_link_titles()
   matches = [t for t in titles if PHRASE.lower() in t.lower()]
   assert len(matches) > 0
-
-  # TODO: Remove this exception once the test is complete
-  raise Exception("Incomplete Test")
