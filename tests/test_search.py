e662ae50db5d05eb8539f31389e824513ce14380
AndyLPK247 <AndyLPK247@gmail.com>
2020-04-10 02:22:15 -0400

    Section 7 code

--- tests/test_search.py
+++ tests/test_search.py
@@ -17,13 +17,14 @@ def test_basic_duckduckgo_search(browser):
   # When the user searches for "panda"
   search_page.search(PHRASE)
 
-  # Then the search result title contains "panda"
-  assert PHRASE in result_page.title()
-  
-  # And the search result query is "panda"
+  # Then the search result query is "panda"
   assert PHRASE == result_page.search_input_value()
   
   # And the search result links pertain to "panda"
   titles = result_page.result_link_titles()
   matches = [t for t in titles if PHRASE.lower() in t.lower()]
   assert len(matches) > 0
+
+  # And the search result title contains "panda"
+  # (Putting this assertion last guarantees that the page title will be ready)
+  assert PHRASE in result_page.title()
