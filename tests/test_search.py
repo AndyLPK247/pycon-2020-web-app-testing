47c45396b5f768a78f7c02eda1b64a3bd7b30883
AndyLPK247 <AndyLPK247@gmail.com>
2020-04-10 02:34:38 -0400

    Section 8 code

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,29 +2,31 @@
 These tests cover DuckDuckGo searches.
 """
 
+import pytest
+
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'polar bear'])
+def test_basic_duckduckgo_search(browser, phrase):
   search_page = DuckDuckGoSearchPage(browser)
   result_page = DuckDuckGoResultPage(browser)
-  PHRASE = "panda"
   
   # Given the DuckDuckGo home page is displayed
   search_page.load()
 
-  # When the user searches for "panda"
-  search_page.search(PHRASE)
+  # When the user searches for the phrase
+  search_page.search(phrase)
 
-  # Then the search result query is "panda"
-  assert PHRASE == result_page.search_input_value()
+  # Then the search result query is the phrase
+  assert phrase == result_page.search_input_value()
   
-  # And the search result links pertain to "panda"
+  # And the search result links pertain to the phrase
   titles = result_page.result_link_titles()
-  matches = [t for t in titles if PHRASE.lower() in t.lower()]
+  matches = [t for t in titles if phrase.lower() in t.lower()]
   assert len(matches) > 0
 
-  # And the search result title contains "panda"
+  # And the search result title contains the phrase
   # (Putting this assertion last guarantees that the page title will be ready)
-  assert PHRASE in result_page.title()
+  assert phrase in result_page.title()
